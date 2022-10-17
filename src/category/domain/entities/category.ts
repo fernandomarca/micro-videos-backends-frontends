fd68e0fd1d26ea858ef9f28696729558ce983835
Fernando Marca Magalhães <fernandomarca@hotmail.com>
2022-10-17 19:02:13 -0300

    validate method

--- src/category/domain/entities/category.ts
+++ src/category/domain/entities/category.ts
@@ -1,3 +1,4 @@
+import { ValidatorRules } from "../../../@seedwork/validators/validator-rules";
 import { Entity } from "../../../@seedwork/domain/entity/entity";
 import { UniqueEntityId } from "../../../@seedwork/domain/value-objects/unique-entity-id.vo";
 
@@ -12,6 +13,7 @@ export class Category extends Entity<CategoryProperties> {
     readonly props: CategoryProperties,
     id?: UniqueEntityId
   ) {
+    Category.validate(props);
     super(props, id);
     this.description = this.props.description;
     this.is_active = this.props.is_active;
@@ -19,8 +21,15 @@ export class Category extends Entity<CategoryProperties> {
   }
 
   update(name: string, description: string) {
+    Category.validate({ name, description });
     this.name = name;
     this.description = description;
+  }
+
+  static validate(props: Omit<CategoryProperties, 'created_at'>) {
+    ValidatorRules.values(props.name, 'name').required().string();
+    ValidatorRules.values(props.description, 'description').string();
+    ValidatorRules.values(props.is_active, 'is_active').boolean();
   }
 
   activate() {
