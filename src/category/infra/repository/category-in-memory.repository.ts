2c02d6e4031d62ce0077427e3a7f7fc07d0d116f
Fernando Marca Magalhães <fernandomarca@hotmail.com>
2022-11-06 14:43:43 -0300

    category repository namespaces

--- src/category/infra/repository/category-in-memory.repository.ts
+++ src/category/infra/repository/category-in-memory.repository.ts
@@ -1,7 +1,18 @@
 import { InMemorySearchableRepository } from "@seedwork/domain/repository/in-memory.repository";
 import { Category } from "category/domain/entities/category";
-import { CategoryRepository } from "category/repository/CategoryRepository";
-
+import CategoryRepository from "category/repository/CategoryRepository";
 class CategoryInMemoryRepository extends
   InMemorySearchableRepository<Category>
-  implements CategoryRepository { }
+  implements CategoryRepository.Repository {
+  protected async applyFilter(
+    items: Category[],
+    filter: CategoryRepository.Filter
+  ): Promise<Category[]> {
+    if (!filter) {
+      return items;
+    }
+    return items.filter((i) => {
+      return i.props.name.toLowerCase().includes(filter.toLowerCase());
+    });
+  }
+}
--- src/category/repository/CategoryRepository.ts
+++ src/category/repository/CategoryRepository.ts
@@ -1,4 +1,36 @@
-import { SearchableRepositoryInterface } from "@seedwork/domain/repository/repository-contracts";
+import {
+  SearchableRepositoryInterface,
+  SearchParams as DefaultSearchParams,
+  SearchResult as DefaultSearchResult
+} from "@seedwork/domain/repository/repository-contracts";
 import { Category } from "category/domain/entities/category";
 
-export interface CategoryRepository extends SearchableRepositoryInterface<Category, any, any> { }
+
+export namespace CategoryRepository {
+  export type Filter = string;
+  export class SearchParams extends DefaultSearchParams<Filter>{ }
+  export class SearchResult extends DefaultSearchResult<
+    Category,
+    Filter
+  >{ }
+
+  export interface Repository extends SearchableRepositoryInterface<
+    Category,
+    Filter,
+    SearchParams,
+    SearchResult> { }
+}
+
+export default CategoryRepository;
+
+
+// export type CategoryFilter = string;
+
+// export class CategorySearchParams extends SearchParams<CategoryFilter>{ }
+
+// export class CategorySearchResult extends SearchResult<
+//   Category,
+//   CategoryFilter
+// >{ }
+
+// export interface CategoryRepository extends SearchableRepositoryInterface<Category, CategoryFilter, CategorySearchParams, CategorySearchResult> { }
