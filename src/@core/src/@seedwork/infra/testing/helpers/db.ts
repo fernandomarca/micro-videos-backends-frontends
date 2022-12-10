4947df87e37344be6098205ee3f33c6d5c754667
Fernando Marca Magalhães <fernandomarca@hotmail.com>
2022-12-10 01:58:26 -0300

    chore: :wrench:
    new config file .env.testing

--- src/@core/src/@seedwork/infra/testing/helpers/db.ts
+++ src/@core/src/@seedwork/infra/testing/helpers/db.ts
@@ -1,9 +1,10 @@
+import { configTest } from "#seedwork/infra/config";
 import { Sequelize, SequelizeOptions } from "sequelize-typescript";
 
 const sequelizeOptions: SequelizeOptions = {
-  dialect: "sqlite",
-  host: ":memory:",
-  logging: false,
+  dialect: configTest.db.vendor,
+  host: configTest.db.host,
+  logging: configTest.db.logging,
 }
 
 export function setupSequelize(options?: SequelizeOptions) {
