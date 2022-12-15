b5849a9622e3704d9db77143ef8dc7c2275eeab5
Fernando Marca Magalhães <fernandomarca@hotmail.com>
2022-12-14 21:26:02 -0300

    create test create category in categories.controller.int-spec

--- src/nestjs/src/categories/__tests__/integration/categories.controller.int-spec.ts
+++ src/nestjs/src/categories/__tests__/integration/categories.controller.int-spec.ts
@@ -1,12 +1,15 @@
 import { CreateCategoryUseCase, DeleteCategoryUseCase, GetCategoryUseCase, ListCategoriesUseCase, UpdateCategoryUseCase } from "@fm/micro-videos/category/application";
+import { CategoryInMemoryRepository } from "@fm/micro-videos/category/infra";
 import { Test, TestingModule } from "@nestjs/testing";
 import { CategoriesController } from "../../../categories/categories.controller";
 import { CategoriesModule } from "../../../categories/categories.module";
 import { ConfigModule } from "../../../config/config.module";
 import { DatabaseModule } from "../../../database/database.module";
+import { CATEGORIES_PROVIDERS } from '../../categories.providers';
 
 describe('CategoriesController Integration tests', () => {
   let controller: CategoriesController;
+  let repository: CategoryInMemoryRepository;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -18,6 +21,7 @@ describe('CategoriesController Integration tests', () => {
     }).compile();
 
     controller = module.get(CategoriesController);
+    repository = module.get(CATEGORIES_PROVIDERS.REPOSITORIES.CATEGORY_REPOSITORY.provide);
   });
 
   it('should be defined', () => {
@@ -27,5 +31,48 @@ describe('CategoriesController Integration tests', () => {
     expect(controller['getCategory']).toBeInstanceOf(GetCategoryUseCase.UseCase);
     expect(controller['deleteCategory']).toBeInstanceOf(DeleteCategoryUseCase.UseCase);
     expect(controller['listCategory']).toBeInstanceOf(ListCategoriesUseCase.UseCase);
+  });
+
+  describe("should create a category", () => {
+
+    const arrange = [
+      {
+        request: {
+          name: 'Movie'
+        },
+        expectedOutput: {
+          name: "Movie",
+          description: null,
+          is_active: true,
+        }
+      },
+      {
+        request: {
+          name: 'Movie',
+          description: 'some description',
+          is_active: false
+        },
+        expectedOutput: {
+          name: 'Movie',
+          description: 'some description',
+          is_active: false
+        }
+      }
+    ];
+
+    test.each(arrange)('with request $request', async ({ request, expectedOutput }) => {
+      const output = await controller.create(request);
+      const entity = await repository.findById(output.id);
+
+      expect(output.id).toBe(entity.id);
+      expect(entity).toMatchObject({
+        id: output.id,
+        name: expectedOutput.name,
+        description: expectedOutput.description,
+        is_active: expectedOutput.is_active,
+        created_at: output.created_at
+      });
+    });
+
   });
 });
