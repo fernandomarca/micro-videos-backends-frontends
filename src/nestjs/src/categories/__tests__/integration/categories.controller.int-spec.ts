2a2295062fc50a3209532c1f9bf48367fa3e679b
Fernando Marca Magalhães <fernandomarca@hotmail.com>
2023-01-05 22:05:05 -0300

    test categories controller

--- src/nestjs/src/categories/__tests__/integration/categories.controller.int-spec.ts
+++ src/nestjs/src/categories/__tests__/integration/categories.controller.int-spec.ts
@@ -1,13 +1,14 @@
 import { CreateCategoryUseCase, DeleteCategoryUseCase, GetCategoryUseCase, ListCategoriesUseCase, UpdateCategoryUseCase } from "@fm/micro-videos/category/application";
-import { CategoryInMemoryRepository, CategorySequelize } from "@fm/micro-videos/category/infra";
+import { CategoryInMemoryRepository } from "@fm/micro-videos/category/infra";
 import { Test, TestingModule } from "@nestjs/testing";
 import { CategoriesController } from "../../../categories/categories.controller";
 import { CategoriesModule } from "../../../categories/categories.module";
 import { ConfigModule } from "../../../config/config.module";
 import { DatabaseModule } from "../../../database/database.module";
 import { CATEGORIES_PROVIDERS } from '../../categories.providers';
-import { NotFoundError } from '@fm/micro-videos/@seedwork/domain';
+import { NotFoundError, SortDirection } from '@fm/micro-videos/@seedwork/domain';
 import { Category } from "@fm/micro-videos/category/domain";
+import { CategoryCollectionPresenter } from "../../../categories/presenter/category.presenter";
 
 describe('CategoriesController Integration tests', () => {
   let controller: CategoriesController;
@@ -146,6 +147,174 @@ describe('CategoriesController Integration tests', () => {
       description: presenter.description,
       is_active: presenter.is_active,
       created_at: presenter.created_at
+    });
+  });
+
+  describe("search method", () => {
+    it('should returns categories using query empty ordered by created_at', async () => {
+      const categories = Category.fake()
+        .theCategories(4)
+        .withName((index) => index + '')
+        .withCreatedAt((index) => new Date(new Date().getTime() + index))
+        .build();
+
+      await repository.bulkInsert(categories);
+
+      const arrange = [
+        {
+          send_data: {},
+          expected: {
+            items: [
+              categories[3],
+              categories[2],
+              categories[1],
+              categories[0],
+            ],
+            current_page: 1,
+            last_page: 1,
+            per_page: 15,
+            total: 4,
+          }
+        },
+        {
+          send_data: { per_page: 2 },
+          expected: {
+            items: [
+              categories[3],
+              categories[2],
+            ],
+            current_page: 1,
+            last_page: 2,
+            per_page: 2,
+            total: 4,
+          }
+        },
+        {
+          send_data: { per_page: 2, page: 2 },
+          expected: {
+            items: [
+              categories[1],
+              categories[0],
+            ],
+            current_page: 2,
+            last_page: 2,
+            per_page: 2,
+            total: 4,
+          }
+        }
+      ];
+
+      for (const item of arrange) {
+        const presenter = await controller.search(item.send_data);
+        expect(presenter).toEqual(
+          new CategoryCollectionPresenter(item.expected)
+        );
+      }
+    });
+
+    it("should returns output using pagination, sort and filter", async () => {
+      const faker = Category.fake().aCategory();
+      const categories = [
+        faker.withName('a').build(),
+        faker.withName('AAA').build(),
+        faker.withName('AaA').build(),
+        faker.withName('b').build(),
+        faker.withName('c').build(),
+      ];
+
+      await repository.bulkInsert(categories);
+
+      const arrange = [
+        {
+          send_data: {
+            page: 1,
+            per_page: 2,
+            sort: 'name',
+            filter: 'a',
+          },
+          expected: {
+            items: [
+              categories[1],
+              categories[2],
+            ],
+            current_page: 1,
+            last_page: 2,
+            per_page: 2,
+            total: 3,
+          } as unknown as ListCategoriesUseCase.Output
+        },
+        {
+          send_data: {
+            page: 2,
+            per_page: 2,
+            sort: 'name',
+            filter: 'a',
+          },
+          expected: {
+            items: [
+              categories[0],
+            ],
+            current_page: 2,
+            last_page: 2,
+            per_page: 2,
+            total: 3,
+          } as unknown as ListCategoriesUseCase.Output
+        }
+      ];
+
+      for (const item of arrange) {
+        const presenter = await controller.search(item.send_data);
+        expect(presenter).toEqual(
+          new CategoryCollectionPresenter(item.expected)
+        );
+      }
+
+      const arrange_desc = [
+        {
+          send_data: {
+            page: 1,
+            per_page: 2,
+            sort: 'name',
+            sort_dir: 'desc' as SortDirection,
+            filter: 'a',
+          },
+          expected: {
+            items: [
+              categories[0],
+              categories[2],
+            ],
+            current_page: 1,
+            last_page: 2,
+            per_page: 2,
+            total: 3,
+          } as unknown as ListCategoriesUseCase.Output
+        },
+        {
+          send_data: {
+            page: 2,
+            per_page: 2,
+            sort: 'name',
+            sort_dir: 'desc' as SortDirection,
+            filter: 'a',
+          },
+          expected: {
+            items: [
+              categories[1],
+            ],
+            current_page: 2,
+            last_page: 2,
+            per_page: 2,
+            total: 3,
+          } as unknown as ListCategoriesUseCase.Output
+        }
+      ];
+
+      for (const item of arrange_desc) {
+        const presenter = await controller.search(item.send_data);
+        expect(presenter).toEqual(
+          new CategoryCollectionPresenter(item.expected)
+        );
+      }
     });
   });
 });
