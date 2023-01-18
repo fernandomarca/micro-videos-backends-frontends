1a6f5b807ef31f93a59f643288b248e3398618aa
Fernando Marca Magalhães <fernandomarca@hotmail.com>
2023-01-17 21:34:46 -0300

    refactor test

--- src/@core/src/@seedwork/domain/entity/entity.spec.ts
+++ src/@core/src/@seedwork/domain/entity/entity.spec.ts
@@ -3,7 +3,15 @@ import { validUUID } from "../utils/validUUID";
 import { UniqueEntityId } from "../value-objects/unique-entity-id.vo";
 import { Entity } from "./entity";
 
-class StubEntity extends Entity<{ prop1: string; prop2: number }>{ }
+class StubEntity extends Entity<{ prop1: string; prop2: number }>{
+  toJSON(): Required<{ id: string; } & { prop1: string; prop2: number; }> {
+    return {
+      id: this.id,
+      prop1: this.props.prop1,
+      prop2: this.props.prop2
+    }
+  }
+}
 describe("Entity Unit Tests", () => {
   it("should set props and id", () => {
     const arrange = { prop1: "value1", prop2: 10 };
