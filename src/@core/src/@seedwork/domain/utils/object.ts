b33564329d8699d98afcd764ef354ddf29763394
Fernando Marca Magalhães <fernandomarca@hotmail.com>
2023-01-17 21:45:01 -0300

    try catch in objects

--- src/@core/src/@seedwork/domain/utils/object.ts
+++ src/@core/src/@seedwork/domain/utils/object.ts
@@ -1,11 +1,15 @@
 export function deepFreeze<T>(obj: T) {
-  const propNames = Object.getOwnPropertyNames(obj);
+  try {
+    const propNames = Object.getOwnPropertyNames(obj);
 
-  for (const name of propNames) {
-    const value = obj[name as keyof T];
-    if (value && typeof value === "object") {
-      deepFreeze(value);
+    for (const name of propNames) {
+      const value = obj[name as keyof T];
+      if (value && typeof value === "object") {
+        deepFreeze(value);
+      }
     }
+    return Object.freeze(obj);
+  } catch (e) {
+    return obj;
   }
-  return Object.freeze(obj);
 }
